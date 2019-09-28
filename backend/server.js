98ad8215722b7e14bea0f87b031f2cccde1276dd
Humberto De Leon <xtealer@xtealer.local>
2019-09-28 10:51:29 -0500

    backend test server

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+// get express server
+const express = require('express');
+// creates new express server
+const app = express();
+// server port where it's going to be listening
+const PORT = 3000;
+
+app.use('/', (req, res, next) => {
+    res.send('ni pingaaaa');
+});
+
+// starts server listening
+app.listen(PORT);
