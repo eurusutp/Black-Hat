e96c652383410c470209e75f60c0b2bf43aa5282
Humberto De Leon <xtealer@xtealer.local>
2019-09-28 12:45:35 -0500

    fixed static files and created static files folder

--- backend/routes/sites.js
+++ backend/routes/sites.js
@@ -6,30 +6,8 @@ const path = require('path');
 const router = express();
 // server routing for pages hosted
 
-// index html
-router.get('/', (req, res, next) => {
-    res.sendFile(path.resolve('..', 'frontend', 'index.html'));
-});
-
-// favicon
-router.get('/favicon.ico', (req, res, next) => {
-    res.sendFile(path.resolve('..', 'frontend', 'favicon.ico'));
-});
-
-// custom stylesheet
-router.get('/css/styles.css', (req, res, next) => {
-    res.sendFile(path.resolve('..', 'frontend', 'css', 'styles.css'));
-});
-
-// site images
-router.get('/assets/images', (req, res, next) => {
-    res.sendFile(path.resolve('..', 'frontend', 'assets', 'images'));
-});
-
-// frontend scripts
-router.get('/assets/js', (req, res, next) => {
-    res.sendFile(path.resolve('..', 'frontend', 'assets', 'js'));
-});
+// static folder
+router.use('/', express.static(path.resolve('..', 'frontend')));
 
 
 module.exports = router;
