f40619ad5b33b34611d9b5ba3fa2e49a5850727c
Humberto De Leon <xtealer@xtealer.local>
2019-09-29 09:17:22 -0500

    git build uploaded

---
+++ react/my-awesome-vis-app/build/precache-manifest.3ddf3968742b9921a5a1b9db86112974.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625108c053399dabc4bcc95a9c796a63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1188236b035938edb07a",
+    "url": "/static/css/main.3f422b8a.chunk.css"
+  },
+  {
+    "revision": "13f1d23472e4e1720ffb",
+    "url": "/static/js/2.a41e8254.chunk.js"
+  },
+  {
+    "revision": "1188236b035938edb07a",
+    "url": "/static/js/main.586acb15.chunk.js"
+  },
+  {
+    "revision": "163c37d9797ed595b574",
+    "url": "/static/js/runtime-main.9245592b.js"
+  },
+  {
+    "revision": "8a7d4e107cdc7103752b2915aa698466",
+    "url": "/static/media/58.8a7d4e10.jpg"
+  },
+  {
+    "revision": "a9b370b0ab661416e5248433a3ddcba2",
+    "url": "/static/media/bad-thumb.a9b370b0.png"
+  },
+  {
+    "revision": "07ff6d4746fe678b4929ea1f896a754e",
+    "url": "/static/media/batteries.07ff6d47.gif"
+  },
+  {
+    "revision": "a2541a0bbe52ed70960566439c048f30",
+    "url": "/static/media/good-thumb.a2541a0b.jpg"
+  },
+  {
+    "revision": "9669611550777bba2f83a90fc01e0634",
+    "url": "/static/media/mercecharge.96696115.jpg"
+  },
+  {
+    "revision": "d10e8ee70313776d33b7c2c1f0835478",
+    "url": "/static/media/recharge.d10e8ee7.jpg"
+  }
+]);
