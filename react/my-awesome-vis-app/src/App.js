6262190c7707c3aee87697a01608ac0dacb3ba71
Jose <jose@freedomstack.com>
2019-09-28 20:50:08 -0500

    -react-app

---
+++ react/my-awesome-vis-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Viz from './vis';
+
+function App() {
+  return (
+    <div className="App">
+      <Viz />
+    </div>
+  );
+}
+
+export default App;
